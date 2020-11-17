5c159bae09e5d6bdc8f82e3f44b9bea6b214e7e3
Stefan Negulescu <tarthedark@gmail.com>
2020-11-17 15:43:54 +0200

    Blazor WebAssembly web client initial commit

---
+++ src/Web/WebBlazor/Server/AppSettings.cs
@@ -0,0 +1,16 @@
+﻿
+namespace WebBlazor.Server
+{
+    public class AppSettings
+    {
+        public string IdentityUrl { get; set; }
+        public string BasketUrl { get; set; }
+        public string MarketingUrl { get; set; }
+
+        public string PurchaseUrl { get; set; }
+        public string SignalrHubUrl { get; set; }
+
+        public string ActivateCampaignDetailFunction { get; set; }
+        public bool UseCustomizationData { get; set; }
+    }
+}
